0cb05d8dff64244eaa56913cace30ea61d9ba973
UnixSupremacist <100294596+unix-supremacist@users.noreply.github.com>
2024-11-12 08:53:04 -0600

    add Os

--- make/cco.mk
+++ make/cco.mk
@@ -1,7 +1,7 @@
 CC := gcc
 clang = -Oz -fno-signed-zeros -freciprocal-math -ffp-contract=fast -fno-trapping-math\
 -ffinite-math-only -ffast-math
-gcc = -fgcse-after-reload -fpeel-loops -funswitch-loops -fipa-cp-clone -fpredictive-commoning\
+gcc = -Os -fgcse-after-reload -fpeel-loops -funswitch-loops -fipa-cp-clone -fpredictive-commoning\
 -fvect-cost-model=dynamic -fversion-loops-for-strides -fallow-store-data-races -ffast-math\
 $(FLOOP) $(FSPLIT) $(FTREE)
 FLOOP = -floop-interchange -floop-unroll-and-jam
